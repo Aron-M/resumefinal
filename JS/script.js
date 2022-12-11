68218d93f8daee212249574e21d321643c0b96b1
Aron-M <aron.moelich88@gmail.com>
2022-12-11 17:16:55 +0000

    creating expanding picture modals for pictures in Where Am I section

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,30 @@ openModal.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
   modal.close();
 });
+
+
+const modal2 = document.querySelector(".building-block-expand");
+const openModal2 = document.querySelector("#bb1");
+const closeModal2 = document.querySelector("#close-modal-2");
+
+openModal2.addEventListener("click", () => {
+  modal2.showModal();
+});
+
+closeModal2.addEventListener("click", () => {
+  modal2.close();
+});
+
+
+const modal3 = document.querySelector(".building-block-expand2");
+const openModal3 = document.querySelector("#bb2");
+const closeModal3 = document.querySelector("#close-modal-3");
+
+openModal3.addEventListener("click", () => {
+  modal3.showModal();
+});
+
+closeModal3.addEventListener("click", () => {
+  modal3.close();
+});
+
